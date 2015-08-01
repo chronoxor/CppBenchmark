1ec8cb493fcc87b6b4e12ecc0f4bef563a2c7760
Ivan Shynkarenka <chronoxor@gmail.com>
2015-08-02 00:43:46 +0300

    Fix Unix build

--- source/environment.cpp
+++ source/environment.cpp
@@ -10,8 +10,10 @@
 
 #include <chrono>
 
-#ifdef _WIN32
+#if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+#include <sys/stat.h>
 #endif
 
 namespace CppBenchmark {
@@ -23,11 +25,16 @@ bool Environment::Is32BitOS()
 
 bool Environment::Is64BitOS()
 {
+#if defined(_WIN32) || defined(_WIN64)
 #if defined(_WIN64)
     return true;
 #elif defined(_WIN32)
     BOOL bWow64Process = FALSE;
     return IsWow64Process(GetCurrentProcess(), &bWow64Process) && bWow64Process;
+#endif
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+    stat buffer;
+    return (stat("/lib64/ld-linux-x86-64.so.2", &buffer) == 0);
 #endif
 }
 
@@ -38,10 +45,18 @@ bool Environment::Is32BitProcess()
 
 bool Environment::Is64BitProcess()
 {
+#if defined(_WIN32) || defined(_WIN64)
 #if defined(_WIN64)
     return true;
 #elif defined(_WIN32)
     return false;
+#endif
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+#if defined(__x86_64__) || defined(__ppc64__)
+    return true;
+#else
+    return false;
+#endif
 #endif
 }
 
