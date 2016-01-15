b2011ff1123f0e345660715ed3190cb94dcc1744
Ivan Shynkarenka <chronoxor@gmail.com>
2016-01-15 03:08:50 +0300

    Remove idea templates

--- .idea/fileTemplates/includes/C File Header.h
+++
@@ -1,9 +0,0 @@
-#if ($HEADER_COMMENTS)
-/*!
-    \file header.h
-    \brief Header definition
-    \author Ivan Shynkarenka
-    \date ${DAY}.${MONTH}.${YEAR}
-    \copyright MIT License
-*/
-#end
--- .idea/fileTemplates/internal/C++ Class.cc
+++
@@ -1,7 +0,0 @@
-#parse("C File Header.h")
-
-#[[#include]]# "${HEADER_FILENAME}"
-
-${NAMESPACES_OPEN}
-
-${NAMESPACES_CLOSE}
