301174fa38bac448151e75c4ca64cd3d02aa1ff0
Ivan Shynkarenka <chronoxor@gmail.com>
2016-08-13 00:10:33 +0300

    Big/Little endians checks

--- tests/test_environment.cpp
+++ tests/test_environment.cpp
@@ -13,6 +13,7 @@ TEST_CASE("Environment information", "[CppBenchmark][Environment]")
     REQUIRE((Environment::Is32BitOS() || Environment::Is64BitOS()));
     REQUIRE((Environment::Is32BitProcess() || Environment::Is64BitProcess()));
     REQUIRE((Environment::IsDebug() || Environment::IsRelease()));
+    REQUIRE((Environment::IsBigEndian() || Environment::IsLittleEndian()));
     REQUIRE(Environment::OSVersion().length() > 0);
     REQUIRE(Environment::Timestamp() > 0);
 }
