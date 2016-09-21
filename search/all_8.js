cae69aed7407347c685be2abdb2bab8cec490865
AppVeyor <team@appveyor.com>
2016-09-21 03:02:44 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['infinite',['infinite',['../class_cpp_benchmark_1_1_settings.html#a4483244712ac587dedb6e680d43a8d9f',1,'CppBenchmark::Settings::infinite() const noexcept'],['../class_cpp_benchmark_1_1_settings.html#a367635efe01172d1dcbed3323e6fe74e',1,'CppBenchmark::Settings::Infinite()']]],
+  ['infinite',['infinite',['../class_cpp_benchmark_1_1_settings.html#a54213522b5e5df79ffd4485c1a8faa3a',1,'CppBenchmark::Settings::infinite() const  noexcept'],['../class_cpp_benchmark_1_1_settings.html#a367635efe01172d1dcbed3323e6fe74e',1,'CppBenchmark::Settings::Infinite()']]],
   ['initbenchmarkcontext',['InitBenchmarkContext',['../class_cpp_benchmark_1_1_benchmark_base.html#a9279e2f3ae93ace0d22ba81e01f896b5',1,'CppBenchmark::BenchmarkBase']]],
   ['initialize',['Initialize',['../class_cpp_benchmark_1_1_fixture.html#a53dc414574cb40710ad95c2266bb607d',1,'CppBenchmark::Fixture::Initialize()'],['../class_cpp_benchmark_1_1_fixture_p_c.html#a24612363ac4bf3f2972f4012b8947f1d',1,'CppBenchmark::FixturePC::Initialize()'],['../class_cpp_benchmark_1_1_fixture_threads.html#a3becfcd255076e3319f5dc51fbcae72c',1,'CppBenchmark::FixtureThreads::Initialize()'],['../class_cpp_benchmark_1_1_launcher_console.html#ab51e0742e808f719303608dbac4bab19',1,'CppBenchmark::LauncherConsole::Initialize()']]],
   ['initializeconsumer',['InitializeConsumer',['../class_cpp_benchmark_1_1_fixture_p_c.html#ae74d35cf9ae57545908124bc18b474bf',1,'CppBenchmark::FixturePC']]],
@@ -14,7 +14,7 @@ var searchData=
   ['isdebug',['IsDebug',['../class_cpp_benchmark_1_1_environment.html#a0b5c4ba6abfd9ef33184527165e7c00c',1,'CppBenchmark::Environment']]],
   ['islittleendian',['IsLittleEndian',['../class_cpp_benchmark_1_1_environment.html#a73c8ff125ef096c34aa9d5a4e43f12de',1,'CppBenchmark::Environment']]],
   ['isrelease',['IsRelease',['../class_cpp_benchmark_1_1_environment.html#a1b3feb2d04c5ec77679f72b87bc2ce9e',1,'CppBenchmark::Environment']]],
-  ['items_5fper_5fsecond',['items_per_second',['../class_cpp_benchmark_1_1_phase_metrics.html#ac6293c3c1467463713e384b73ee29599',1,'CppBenchmark::PhaseMetrics']]],
-  ['iterations',['Iterations',['../class_cpp_benchmark_1_1_settings.html#ab51585f582894997005acebef1f89cc2',1,'CppBenchmark::Settings::Iterations(int64_t iterations)'],['../class_cpp_benchmark_1_1_settings.html#ae62dfcf9dce88a0d85df424490b42805',1,'CppBenchmark::Settings::iterations() const noexcept']]],
-  ['iterations_5fper_5fsecond',['iterations_per_second',['../class_cpp_benchmark_1_1_phase_metrics.html#a1687e7e7e1d695ed4d847cf5a1584897',1,'CppBenchmark::PhaseMetrics']]]
+  ['items_5fper_5fsecond',['items_per_second',['../class_cpp_benchmark_1_1_phase_metrics.html#a2a078ab76921722947cb0602eaba1c9d',1,'CppBenchmark::PhaseMetrics']]],
+  ['iterations',['Iterations',['../class_cpp_benchmark_1_1_settings.html#ab51585f582894997005acebef1f89cc2',1,'CppBenchmark::Settings::Iterations(int64_t iterations)'],['../class_cpp_benchmark_1_1_settings.html#af58d505ca352673e168ec7d175b9bcd1',1,'CppBenchmark::Settings::iterations() const  noexcept']]],
+  ['iterations_5fper_5fsecond',['iterations_per_second',['../class_cpp_benchmark_1_1_phase_metrics.html#afa317ebac70179b9d289885779368688',1,'CppBenchmark::PhaseMetrics']]]
 ];
