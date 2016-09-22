c86a5cfb8c2b3e8f7444f6c49c2242cd70bcf471
Travis CI <travis@travis-ci.org>
2016-09-22 00:32:01 +0000

    Update generated documentation

--- search/all_a.js
+++ search/all_a.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['magenta',['MAGENTA',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69ac634ffea7195608364671ac52ee59a61',1,'CppBenchmark']]],
-  ['max_5ftime',['max_time',['../class_cpp_benchmark_1_1_phase_metrics.html#ac18082158ba5f3f582c3b50856e3fc4a',1,'CppBenchmark::PhaseMetrics']]],
+  ['max_5ftime',['max_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a82cff61d5e76291d3ce3ba06d44c4593',1,'CppBenchmark::PhaseMetrics']]],
   ['mergemetrics',['MergeMetrics',['../class_cpp_benchmark_1_1_phase_core.html#afac9a558c8962f0dab07181ac725032f',1,'CppBenchmark::PhaseCore::MergeMetrics()'],['../class_cpp_benchmark_1_1_phase_core.html#a79484aac45e4484819e592a8477c983e',1,'CppBenchmark::PhaseCore::MergeMetrics(const PhaseCore &amp;phase)']]],
-  ['metrics',['metrics',['../class_cpp_benchmark_1_1_context.html#a7f3b6da52954eb49b791ebce41e53e16',1,'CppBenchmark::Context::metrics() noexcept'],['../class_cpp_benchmark_1_1_context.html#aa86c209ce8c1ef97ffa6072aab52e47c',1,'CppBenchmark::Context::metrics() const  noexcept override'],['../class_cpp_benchmark_1_1_phase.html#a30cd69a290ad87fa4a99931dd47af588',1,'CppBenchmark::Phase::metrics()'],['../class_cpp_benchmark_1_1_phase_core.html#ab80f20b0b4357c122a1ae3cd91c27cce',1,'CppBenchmark::PhaseCore::metrics()'],['../class_cpp_benchmark_1_1_phase_scope.html#a1b4f520dc5546b6af0a9812a6e3ec163',1,'CppBenchmark::PhaseScope::metrics()']]],
-  ['min_5ftime',['min_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a7f6616ddf445f5f0ecda2fdeec2b249c',1,'CppBenchmark::PhaseMetrics']]]
+  ['metrics',['metrics',['../class_cpp_benchmark_1_1_context.html#a7f3b6da52954eb49b791ebce41e53e16',1,'CppBenchmark::Context::metrics() noexcept'],['../class_cpp_benchmark_1_1_context.html#a842a1e4b7f40ceb8cfa16a8ef53b1b4a',1,'CppBenchmark::Context::metrics() const noexcept override'],['../class_cpp_benchmark_1_1_phase.html#a86e4e8927f733d3fe00040cd57e588a0',1,'CppBenchmark::Phase::metrics()'],['../class_cpp_benchmark_1_1_phase_core.html#ab97edc1b2081d029ccbae5faad579216',1,'CppBenchmark::PhaseCore::metrics()'],['../class_cpp_benchmark_1_1_phase_scope.html#a7f5b6853cb7af6446b7dca197fcda2cf',1,'CppBenchmark::PhaseScope::metrics()']]],
+  ['min_5ftime',['min_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a32ebb07fdbff7d502db371abe88d1561',1,'CppBenchmark::PhaseMetrics']]]
 ];
