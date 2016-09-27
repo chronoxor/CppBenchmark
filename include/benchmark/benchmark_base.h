e2a4c5af3f03d38f326f4555c0e3cd4ab3a38bd8
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-28 02:03:46 +0300

    update

--- include/benchmark/benchmark_base.h
+++ include/benchmark/benchmark_base.h
@@ -57,20 +57,60 @@ public:
     const Settings& settings() const { return _settings; }
 
 protected:
+    //! Benchmark launched flag
     bool _launched;
+    //! Benchmark name
     std::string _name;
+    //! Benchmark settings
     Settings _settings;
+    //! Benchmark phases
     std::vector<std::shared_ptr<PhaseCore>> _phases;
 
+    //! Get the count of benchmark launches
+    /*!
+        \return Benchmark launches count
+    */
     virtual int CountLaunches() const { return 1; }
+
+    //! Get the count of benchmark launches
+    /*!
+        \param current - Current benchmark number
+        \param total - Total benchmarks
+        \param handler - Launcher handler
+    */
     virtual void Launch(int& current, int total, LauncherHandler& handler) {}
 
+    //! Initialize benchmark context
+    /*!
+        \param context - Benchmark context
+    */
     void InitBenchmarkContext(Context& context);
 
+    //! Update benchmark metrics for the given benchmark phases collection
+    /*!
+        \param phases - Benchmark phases collection
+    */
     static void UpdateBenchmarkMetrics(std::vector<std::shared_ptr<PhaseCore>>& phases);
+    //! Update benchmark metrics for the given benchmark phase
+    /*!
+        \param phase - Benchmark phase
+    */
     static void UpdateBenchmarkMetrics(PhaseCore& phase);
+    //! Update benchmark threads metrics for the given benchmark phases collection
+    /*!
+        \param phases - Benchmark phases collection
+    */
     static void UpdateBenchmarkThreads(std::vector<std::shared_ptr<PhaseCore>>& phases);
+    //! Update benchmark names for the given benchmark phases collection
+    /*!
+        \param phases - Benchmark phases collection
+    */
     static void UpdateBenchmarkNames(std::vector<std::shared_ptr<PhaseCore>>& phases);
+    //! Update benchmark names for the given benchmark phase
+    /*!
+        \param phase - Benchmark phase
+        \param name - Benchmark name
+    */
     static void UpdateBenchmarkNames(PhaseCore& phase, const std::string& name);
 };
 
