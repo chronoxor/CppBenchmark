c18018ca01ea6284b8ea96ca1983e6006c53c1b4
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-30 01:54:30 +0300

    update

--- modules/cpp-optparse.cmake
+++ modules/cpp-optparse.cmake
@@ -1,4 +1,9 @@
 if(NOT TARGET cpp-optparse)
+
+  # Add module library
   add_library(cpp-optparse cpp-optparse/OptionParser.h cpp-optparse/OptionParser.cpp)
+
+  # Set module folder
   set_target_properties(cpp-optparse PROPERTIES FOLDER modules/cpp-optparse)
+
 endif()
