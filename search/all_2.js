3fe2ae67971c79161bd2df113d93f7563d6cac3d
Travis CI <travis@travis-ci.org>
2016-09-29 22:00:47 +0000

    Update generated documentation

--- search/all_2.js
+++ search/all_2.js
@@ -37,5 +37,5 @@ var searchData=
   ['black',['BLACK',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a08d0012388564e95c3b4a7407cf04965',1,'CppBenchmark']]],
   ['blue',['BLUE',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a1b3e1ee9bff86431dea6b181365ba65f',1,'CppBenchmark']]],
   ['brown',['BROWN',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a493cacf6f6a2ae4798b319b8b9ba9488',1,'CppBenchmark']]],
-  ['bytes_5fper_5fsecond',['bytes_per_second',['../class_cpp_benchmark_1_1_phase_metrics.html#a7b3e5a7ba016462e42ed820eab57f693',1,'CppBenchmark::PhaseMetrics']]]
+  ['bytes_5fper_5fsecond',['bytes_per_second',['../class_cpp_benchmark_1_1_phase_metrics.html#ac5ac97efaa647406625a4e358d1aa646',1,'CppBenchmark::PhaseMetrics']]]
 ];
