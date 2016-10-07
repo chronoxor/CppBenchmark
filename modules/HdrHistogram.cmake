58bca2500289a66b043a92081fc5af1636955060
Ivan Shynkarenka <chronoxor@gmail.com>
2016-10-08 02:52:08 +0300

    Add support of HrdHistograms

---
+++ modules/HdrHistogram.cmake
@@ -0,0 +1,25 @@
+if(NOT TARGET histogram)
+
+  # Includes
+  include_directories("zlib")
+  include_directories("../build/modules/zlib")
+
+  # Sources
+  file(GLOB_RECURSE SOURCE_FILES "HdrHistogram/src/*.c")
+
+  # Temporary disable some warnings
+  # C4146: unary minus operator applied to unsigned type, result still unsigned
+  # C4200: nonstandard extension used : zero-sized array in struct/union
+  # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
+  if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "/wd4146 /wd4200 /wd4244")
+  endif()
+
+  # Add module library
+  add_library(histogram ${SOURCE_FILES})
+  target_link_libraries(histogram zlibstatic)
+
+  # Set module folder
+  set_target_properties(histogram PROPERTIES FOLDER modules/HdrHistogram)
+
+endif()
---
+++ modules/zlib.cmake
@@ -0,0 +1,46 @@
+if(NOT TARGET zlib)
+
+  # Temporary disable some warnings
+  # C4127: conditional expression is constant
+  # C4131: 'function' : uses old-style declarator
+  # C4210: nonstandard extension used : function given file scope
+  # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
+  if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
+    set(CMAKE_OLD_C_FLAGS ${CMAKE_C_FLAGS})
+    set(CMAKE_OLD_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4127 /wd4131 /wd4210 /wd4244")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4127 /wd4131 /wd4210 /wd4244")
+  endif()
+
+  # Set module options
+  option(AMD64 "Enable building amd64 assembly implementation" ON)
+
+  # Set install flag
+  set(SKIP_INSTALL_ALL Y)
+
+  # Includes
+  include_directories("zlib")
+  include_directories("../build/modules/zlib")
+
+  # Add module subdirectory
+  add_subdirectory("zlib")
+
+  # Add missing target sources
+  if(MSVC AND AMD64)
+    target_sources(zlib PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/zlib/contrib/masmx64/inffas8664.c")
+    target_sources(zlibstatic PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/zlib/contrib/masmx64/inffas8664.c")
+  endif()
+
+  # Set module folder
+  set_target_properties(zlib zlibstatic example minigzip PROPERTIES FOLDER modules/zlib)
+
+  # Exclude some modules from the build
+  set_target_properties(example minigzip PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
+
+  # Restore default warnings
+  if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
+    set(CMAKE_C_FLAGS ${CMAKE_OLD_C_FLAGS})
+    set(CMAKE_CXX_FLAGS ${CMAKE_OLD_CXX_FLAGS})
+  endif()
+
+endif()
