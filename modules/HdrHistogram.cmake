fb731f9a5015b6ef28e4a54f4d12fae125b1490a
Ivan Shynkarenka <chronoxor@gmail.com>
2016-10-09 01:50:29 +0300

    update

--- modules/HdrHistogram.cmake
+++ modules/HdrHistogram.cmake
@@ -8,12 +8,7 @@ if(NOT TARGET histogram)
   # C4200: nonstandard extension used : zero-sized array in struct/union
   # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
   if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "/wd4146 /wd4200 /wd4244")
-  elseif(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC)
-    set(CMAKE_OLD_C_FLAGS ${CMAKE_C_FLAGS})
-    set(CMAKE_OLD_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_INIT} -Wall -Werror" FORCE)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_INIT} -Wall -Werror" FORCE)
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${COMMON_COMPILE_FLAGS} /wd4146 /wd4200 /wd4244")
   endif()
 
   # Includes
