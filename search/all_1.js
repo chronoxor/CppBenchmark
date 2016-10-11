d7f5dc197898d101b42f694c4d0c81bb7eae14fd
AppVeyor <team@appveyor.com>
2016-10-11 15:01:52 +0000

    Update generated documentation

--- search/all_1.js
+++ search/all_1.js
@@ -6,6 +6,6 @@ var searchData=
   ['additems',['AddItems',['../class_cpp_benchmark_1_1_phase_metrics.html#a91dd543cfcffb02cacbbd966d38ac402',1,'CppBenchmark::PhaseMetrics']]],
   ['additerations',['AddIterations',['../class_cpp_benchmark_1_1_phase_metrics.html#a4a40af3a716824e3a77229531939f546',1,'CppBenchmark::PhaseMetrics']]],
   ['addlatency',['AddLatency',['../class_cpp_benchmark_1_1_phase_metrics.html#ab48431555aa8f92bc1d6362a5959866b',1,'CppBenchmark::PhaseMetrics']]],
-  ['attempts',['attempts',['../class_cpp_benchmark_1_1_settings.html#a32a96f920f1d672a94e416d6c31f136d',1,'CppBenchmark::Settings::attempts() const noexcept'],['../class_cpp_benchmark_1_1_settings.html#a0c3031c2a828600205384c067c1c6170',1,'CppBenchmark::Settings::Attempts(int attempts)']]],
-  ['avg_5ftime',['avg_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a7c1eccbcd80fa51b6f7d32aca41e4424',1,'CppBenchmark::PhaseMetrics']]]
+  ['attempts',['attempts',['../class_cpp_benchmark_1_1_settings.html#a77bd9d0186ffeafcb4f3c72d1c83635e',1,'CppBenchmark::Settings::attempts() const  noexcept'],['../class_cpp_benchmark_1_1_settings.html#a0c3031c2a828600205384c067c1c6170',1,'CppBenchmark::Settings::Attempts(int attempts)']]],
+  ['avg_5ftime',['avg_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a3fccdf93743e295d560f7cef4e2b9fae',1,'CppBenchmark::PhaseMetrics']]]
 ];
