0642634cb335a3b2a86fd8a13dd4eaaa0c336fab
Ivan Shynkarenka <chronoxor@gmail.com>
2017-01-03 13:41:41 +0300

    Fix zlib issues

--- modules/zlib.cmake
+++ modules/zlib.cmake
@@ -38,17 +38,6 @@ if(NOT TARGET zlib)
     add_definitions(-D_CRT_NONSTDC_NO_DEPRECATE)
   endif()
 
-  # Module assembler files
-  if(NOT APPLE AND NOT CYGWIN AND NOT MINGW AND NOT MSVC)
-    add_definitions(-DASMV)
-    set(ASSEMBLER_FILES "zlib/contrib/amd64/amd64-match.S")
-    set_source_files_properties(${ASSEMBLER_FILES} PROPERTIES LANGUAGE C COMPILE_FLAGS -DNO_UNDERLINE)
-  elseif(MSVC)
-    ENABLE_LANGUAGE(ASM_MASM)
-    add_definitions(-DASMV -DASMINF)
-    set(ASSEMBLER_FILES "zlib/contrib/masmx64/gvmat64.asm" "zlib/contrib/masmx64/inffasx64.asm")
-  endif()
-
   # Module library
   file(GLOB SOURCE_FILES "zlib/*.c")
   if(NOT MSVC)
@@ -58,8 +47,10 @@ if(NOT TARGET zlib)
     # C4131: 'function' : uses old-style declarator
     # C4210: nonstandard extension used : function given file scope
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
+    # C4245: 'conversion' : conversion from 'type1' to 'type2', signed/unsigned mismatch
+    # C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
     set(${SOURCE_FILES} "zlib/contrib/masmx64/inffas8664.c" "zlib/contrib/masmx64/inffas8664.c")
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4131 /wd4210 /wd4244")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4131 /wd4210 /wd4244 /wd4245 /wd4267")
   endif()
   add_library(zlib ${SOURCE_FILES} ${ASSEMBLER_FILES})
   target_link_libraries(zlib)
