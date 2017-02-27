15096cdfb9ec2a9e47069116a580b16aa08943fb
AppVeyor <team@appveyor.com>
2017-02-27 22:50:24 +0000

    Update generated documentation

--- search/functions_9.js
+++ search/functions_9.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['max_5flatency',['max_latency',['../class_cpp_benchmark_1_1_phase_metrics.html#a4b7e49b9a03b45424fda81153bd7fd61',1,'CppBenchmark::PhaseMetrics']]],
-  ['max_5ftime',['max_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a82cff61d5e76291d3ce3ba06d44c4593',1,'CppBenchmark::PhaseMetrics']]],
-  ['mean_5flatency',['mean_latency',['../class_cpp_benchmark_1_1_phase_metrics.html#a10e14d2699653d7cfaae8d6893b6bf9e',1,'CppBenchmark::PhaseMetrics']]],
+  ['max_5flatency',['max_latency',['../class_cpp_benchmark_1_1_phase_metrics.html#a8a51a1e24cb53ec406d0a970a8e92455',1,'CppBenchmark::PhaseMetrics']]],
+  ['max_5ftime',['max_time',['../class_cpp_benchmark_1_1_phase_metrics.html#ac18082158ba5f3f582c3b50856e3fc4a',1,'CppBenchmark::PhaseMetrics']]],
+  ['mean_5flatency',['mean_latency',['../class_cpp_benchmark_1_1_phase_metrics.html#a2e409506bed9e981212cbc1271d663ad',1,'CppBenchmark::PhaseMetrics']]],
   ['mergemetrics',['MergeMetrics',['../class_cpp_benchmark_1_1_phase_core.html#afac9a558c8962f0dab07181ac725032f',1,'CppBenchmark::PhaseCore::MergeMetrics()'],['../class_cpp_benchmark_1_1_phase_core.html#addc835feeb6cd78b92e60fa1a4e1fccb',1,'CppBenchmark::PhaseCore::MergeMetrics(PhaseCore &amp;phase)']]],
-  ['metrics',['metrics',['../class_cpp_benchmark_1_1_context.html#a7f3b6da52954eb49b791ebce41e53e16',1,'CppBenchmark::Context::metrics() noexcept'],['../class_cpp_benchmark_1_1_context.html#a842a1e4b7f40ceb8cfa16a8ef53b1b4a',1,'CppBenchmark::Context::metrics() const noexcept override'],['../class_cpp_benchmark_1_1_phase.html#a86e4e8927f733d3fe00040cd57e588a0',1,'CppBenchmark::Phase::metrics()'],['../class_cpp_benchmark_1_1_phase_core.html#ab97edc1b2081d029ccbae5faad579216',1,'CppBenchmark::PhaseCore::metrics()'],['../class_cpp_benchmark_1_1_phase_scope.html#a7f5b6853cb7af6446b7dca197fcda2cf',1,'CppBenchmark::PhaseScope::metrics()']]],
-  ['min_5flatency',['min_latency',['../class_cpp_benchmark_1_1_phase_metrics.html#a436368acaaa3de1e8938299917b2d694',1,'CppBenchmark::PhaseMetrics']]],
-  ['min_5ftime',['min_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a32ebb07fdbff7d502db371abe88d1561',1,'CppBenchmark::PhaseMetrics']]]
+  ['metrics',['metrics',['../class_cpp_benchmark_1_1_context.html#a7f3b6da52954eb49b791ebce41e53e16',1,'CppBenchmark::Context::metrics() noexcept'],['../class_cpp_benchmark_1_1_context.html#aa86c209ce8c1ef97ffa6072aab52e47c',1,'CppBenchmark::Context::metrics() const  noexcept override'],['../class_cpp_benchmark_1_1_phase.html#a30cd69a290ad87fa4a99931dd47af588',1,'CppBenchmark::Phase::metrics()'],['../class_cpp_benchmark_1_1_phase_core.html#ab80f20b0b4357c122a1ae3cd91c27cce',1,'CppBenchmark::PhaseCore::metrics()'],['../class_cpp_benchmark_1_1_phase_scope.html#a1b4f520dc5546b6af0a9812a6e3ec163',1,'CppBenchmark::PhaseScope::metrics()']]],
+  ['min_5flatency',['min_latency',['../class_cpp_benchmark_1_1_phase_metrics.html#a32154141ab644a28c733041efcfdaad9',1,'CppBenchmark::PhaseMetrics']]],
+  ['min_5ftime',['min_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a7f6616ddf445f5f0ecda2fdeec2b249c',1,'CppBenchmark::PhaseMetrics']]]
 ];
