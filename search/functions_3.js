75c63a6c900e33eabff7a4e654988b4eff9ec202
Travis CI <travis@travis-ci.org>
2017-05-05 20:21:06 +0000

    Update generated documentation

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['description',['description',['../class_cpp_benchmark_1_1_context.html#a83d114ef0ba9bc483db52fc2ec085f42',1,'CppBenchmark::Context::description()'],['../class_cpp_benchmark_1_1_context_p_c.html#ad6c2152e9bb8b42d2b75652a975b24f7',1,'CppBenchmark::ContextPC::description()'],['../class_cpp_benchmark_1_1_context_threads.html#a5f8016f0ba3150595607ad3ce7825f46',1,'CppBenchmark::ContextThreads::description()']]]
+  ['description',['description',['../class_cpp_benchmark_1_1_context.html#a06d58fdbd83961bf86336f3e10637f9d',1,'CppBenchmark::Context::description()'],['../class_cpp_benchmark_1_1_context_p_c.html#ad6c2152e9bb8b42d2b75652a975b24f7',1,'CppBenchmark::ContextPC::description()'],['../class_cpp_benchmark_1_1_context_threads.html#a5f8016f0ba3150595607ad3ce7825f46',1,'CppBenchmark::ContextThreads::description()']]]
 ];
