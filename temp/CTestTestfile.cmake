daf3a4a8b0826f48a65ebba2d0d5b2ccfb61e03e
Ivan Shynkarenka <chronoxor@gmail.com>
2017-07-19 21:35:18 +0300

    Fix gh-pages branch

--- temp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: Z:/Projects/CppProjects/CppBenchmark
-# Build directory: Z:/Projects/CppProjects/CppBenchmark/temp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cppbenchmark-tests "cppbenchmark-tests" "--durations" "yes" "--order" "lex")
-subdirs("modules")
