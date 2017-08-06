cf55120f460e7255cd5f82923ca22f51f5d3b70d
Ivan Shynkarenka <chronoxor@gmail.com>
2017-08-06 17:51:50 +0300

    Submodule Sync

--- examples/sleep.cpp
+++ examples/sleep.cpp
@@ -1,18 +1,7 @@
-//
-// Created by Ivan Shynkarenka on 11.10.2016
-//
+#include <windows.h>
 
-#include "benchmark/cppbenchmark.h"
-
-#include <chrono>
-#include <thread>
-
-const uint64_t iterations = 100;
-const auto settings = CppBenchmark::Settings().Iterations(iterations).Latency(1, 1000000000, 5);
-
-BENCHMARK("sleep", settings)
+void main()
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    while (true)
+        Sleep(1000);
 }
-
-BENCHMARK_MAIN()
