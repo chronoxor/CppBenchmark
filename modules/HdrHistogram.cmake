d64d77f39e2de1457817c5fedb15376c431ecd0f
Ivan Shynkarenka <chronoxor@gmail.com>
2018-02-12 20:14:21 +0300

    update

--- modules/HdrHistogram.cmake
+++ modules/HdrHistogram.cmake
@@ -6,9 +6,8 @@ if(NOT TARGET HdrHistogram)
     set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${COMMON_COMPILE_FLAGS} -Wno-implicit-function-declaration -Wno-incompatible-pointer-types")
   else()
     # C4146: unary minus operator applied to unsigned type, result still unsigned
-    # C4200: nonstandard extension used : zero-sized array in struct/union
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4146 /wd4200 /wd4244")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4146 /wd4244")
   endif()
   add_library(HdrHistogram ${SOURCE_FILES})
   target_include_directories(HdrHistogram PRIVATE "zlib" PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/zlib")
