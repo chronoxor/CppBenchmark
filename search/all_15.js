d1e55bc923e92fede0cb708b61c41101c80ca90a
Travis CI <travis@travis-ci.org>
2018-04-03 18:20:21 +0000

    Update generated documentation

--- search/all_15.js
+++ search/all_15.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['y',['y',['../class_cpp_benchmark_1_1_context.html#a5d13da5fe2ad99148ce315cd6982e255',1,'CppBenchmark::Context']]],
+  ['y',['y',['../class_cpp_benchmark_1_1_context.html#a9a3a359ea78b60c144d82d43454ecddc',1,'CppBenchmark::Context']]],
   ['yellow',['YELLOW',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a8a568e5f41b7e4da88fe5c4a00aad34e',1,'CppBenchmark']]]
 ];
