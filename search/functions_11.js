d22d2623413d5efb88c020f3a522834885c11d5f
Travis CI <travis@travis-ci.org>
2018-05-03 18:26:50 +0000

    Update generated documentation

--- search/functions_11.js
+++ search/functions_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wait',['Wait',['../class_cpp_benchmark_1_1_barrier.html#ada326f3dcf828fc84b1328d7fd2f43c0',1,'CppBenchmark::Barrier']]]
+  ['wait',['Wait',['../class_cpp_benchmark_1_1_barrier.html#ada326f3dcf828fc84b1328d7fd2f43c0',1,'CppBenchmark::Barrier']]],
+  ['windowsendline',['WindowsEndLine',['../class_cpp_benchmark_1_1_environment.html#a8c3597418a3943df248e2bb2a2586a35',1,'CppBenchmark::Environment']]]
 ];
