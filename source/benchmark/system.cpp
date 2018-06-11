500c794d00d47958a24e4d38e3cf202481ab33bf
Ivan Shynkarenka <chronoxor@gmail.com>
2018-06-11 18:49:19 +0300

    Submodule Sync

--- source/benchmark/system.cpp
+++ source/benchmark/system.cpp
@@ -138,11 +138,7 @@ std::string System::CpuArchitecture()
     static std::regex pattern("model name(.*): (.*)");
 
     std::string line;
-#if defined(__CYGWIN__)
-    std::ifstream stream("/proc/cpuinfo\n");
-#else
     std::ifstream stream("/proc/cpuinfo");
-#endif
     while (getline(stream, line))
     {
         std::smatch matches;
@@ -269,11 +265,7 @@ int64_t System::CpuClockSpeed()
     static std::regex pattern("cpu MHz(.*): (.*)");
 
     std::string line;
-#if defined(__CYGWIN__)
-    std::ifstream stream("/proc/cpuinfo\n");
-#else
     std::ifstream stream("/proc/cpuinfo");
-#endif
     while (getline(stream, line))
     {
         std::smatch matches;
