e3267d5e370d9367bfc6b9e537214d56909503d8
Travis CI <travis@travis-ci.org>
2018-07-19 12:41:25 +0000

    Update generated documentation

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['darkgrey',['DARKGREY',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a571a40100f7bd18fda2191490111e6cf',1,'CppBenchmark']]],
-  ['description',['description',['../class_cpp_benchmark_1_1_context.html#a06d58fdbd83961bf86336f3e10637f9d',1,'CppBenchmark::Context::description()'],['../class_cpp_benchmark_1_1_context_p_c.html#ad6c2152e9bb8b42d2b75652a975b24f7',1,'CppBenchmark::ContextPC::description()'],['../class_cpp_benchmark_1_1_context_threads.html#a5f8016f0ba3150595607ad3ce7825f46',1,'CppBenchmark::ContextThreads::description()']]],
+  ['description',['description',['../class_cpp_benchmark_1_1_context.html#a83d114ef0ba9bc483db52fc2ec085f42',1,'CppBenchmark::Context::description()'],['../class_cpp_benchmark_1_1_context_p_c.html#ad6c2152e9bb8b42d2b75652a975b24f7',1,'CppBenchmark::ContextPC::description()'],['../class_cpp_benchmark_1_1_context_threads.html#a5f8016f0ba3150595607ad3ce7825f46',1,'CppBenchmark::ContextThreads::description()']]],
   ['duration',['duration',['../class_cpp_benchmark_1_1_settings.html#a3d7a83792b17e3a6e273fd8277b0e03e',1,'CppBenchmark::Settings::duration() const noexcept'],['../class_cpp_benchmark_1_1_settings.html#aad7254ebd452d53adcee2e5011bccbcf',1,'CppBenchmark::Settings::Duration(int64_t duration)']]]
 ];
