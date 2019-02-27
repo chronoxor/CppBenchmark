0dc5b75d9efd67c88b7930c15bc46c95ec7cbe7b
Ivan Shynkarenka <chronoxor@gmail.com>
2019-02-27 17:50:38 +0300

    update

--- source/benchmark/launcher_console.cpp
+++ source/benchmark/launcher_console.cpp
@@ -27,7 +27,6 @@ void LauncherConsole::Initialize(const int argc, char const* const* const argv)
 
     const char* output[] = { "console", "csv", "json" };
 
-    parser.add_option("-h", "--help").help("Show help");
     parser.add_option("-f", "--filter").help("Filter benchmarks by the given regexp pattern");
     parser.add_option("-l", "--list").action("store_true").help("List all avaliable benchmarks");
     parser.add_option("-o", "--output").choices(&output[0], &output[3]).set_default(output[0]).help("Output format (console, csv, json). Default: %default");
