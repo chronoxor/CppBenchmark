ec2ee70a30c945e0de9dbbfb1f6f842b0215617c
Ivan Shynkarenka <chronoxor@gmail.com>
2019-07-28 20:49:31 +0300

    Fixing compilation errors

--- modules/HdrHistogram.cmake
+++ modules/HdrHistogram.cmake
@@ -3,9 +3,10 @@ if(NOT TARGET HdrHistogram)
   # Module library
   file(GLOB SOURCE_FILES "HdrHistogram/src/*.c")
   if(MSVC)
+    # C4057: 'operator' : 'identifier1' indirection to slightly different base types from 'identifier2'
     # C4146: unary minus operator applied to unsigned type, result still unsigned
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4146 /wd4244")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4057 /wd4146 /wd4244")
   else()
     set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-format -Wno-implicit-function-declaration -Wno-incompatible-pointer-types")
   endif()
