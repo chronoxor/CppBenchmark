1288f7b905d7885234fcdc167f6723bb47b5a91f
Travis CI <travis@travis-ci.org>
2020-01-14 13:07:37 +0000

    Update generated documentation

--- search/all_1.js
+++ search/all_1.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['addbenchmark',['AddBenchmark',['../class_cpp_benchmark_1_1_launcher.html#affcfa233d35b46366a3ea0890a2f6b8a',1,'CppBenchmark::Launcher']]],
-  ['addbenchmarkbuilder',['AddBenchmarkBuilder',['../class_cpp_benchmark_1_1_launcher.html#a6fd17f985be7d97e3d294b1cd3fbcfa6',1,'CppBenchmark::Launcher']]],
-  ['addbytes',['AddBytes',['../class_cpp_benchmark_1_1_phase_metrics.html#ab18695107bd30df57c21e6f289982085',1,'CppBenchmark::PhaseMetrics']]],
-  ['additems',['AddItems',['../class_cpp_benchmark_1_1_phase_metrics.html#a91dd543cfcffb02cacbbd966d38ac402',1,'CppBenchmark::PhaseMetrics']]],
-  ['addlatency',['AddLatency',['../class_cpp_benchmark_1_1_phase_metrics.html#ab48431555aa8f92bc1d6362a5959866b',1,'CppBenchmark::PhaseMetrics']]],
-  ['addoperations',['AddOperations',['../class_cpp_benchmark_1_1_phase_metrics.html#a32d5f18caac940fe52cb2ce3f09dc1bc',1,'CppBenchmark::PhaseMetrics']]],
-  ['attempts',['attempts',['../class_cpp_benchmark_1_1_settings.html#a32a96f920f1d672a94e416d6c31f136d',1,'CppBenchmark::Settings::attempts() const noexcept'],['../class_cpp_benchmark_1_1_settings.html#a0c3031c2a828600205384c067c1c6170',1,'CppBenchmark::Settings::Attempts(int attempts)']]],
-  ['avg_5ftime',['avg_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a7c1eccbcd80fa51b6f7d32aca41e4424',1,'CppBenchmark::PhaseMetrics']]]
+  ['addbenchmark_22',['AddBenchmark',['../class_cpp_benchmark_1_1_launcher.html#affcfa233d35b46366a3ea0890a2f6b8a',1,'CppBenchmark::Launcher']]],
+  ['addbenchmarkbuilder_23',['AddBenchmarkBuilder',['../class_cpp_benchmark_1_1_launcher.html#a6fd17f985be7d97e3d294b1cd3fbcfa6',1,'CppBenchmark::Launcher']]],
+  ['addbytes_24',['AddBytes',['../class_cpp_benchmark_1_1_phase_metrics.html#ab18695107bd30df57c21e6f289982085',1,'CppBenchmark::PhaseMetrics']]],
+  ['additems_25',['AddItems',['../class_cpp_benchmark_1_1_phase_metrics.html#a91dd543cfcffb02cacbbd966d38ac402',1,'CppBenchmark::PhaseMetrics']]],
+  ['addlatency_26',['AddLatency',['../class_cpp_benchmark_1_1_phase_metrics.html#ab48431555aa8f92bc1d6362a5959866b',1,'CppBenchmark::PhaseMetrics']]],
+  ['addoperations_27',['AddOperations',['../class_cpp_benchmark_1_1_phase_metrics.html#a32d5f18caac940fe52cb2ce3f09dc1bc',1,'CppBenchmark::PhaseMetrics']]],
+  ['attempts_28',['attempts',['../class_cpp_benchmark_1_1_settings.html#a32a96f920f1d672a94e416d6c31f136d',1,'CppBenchmark::Settings::attempts() const noexcept'],['../class_cpp_benchmark_1_1_settings.html#a0c3031c2a828600205384c067c1c6170',1,'CppBenchmark::Settings::Attempts(int attempts)']]],
+  ['avg_5ftime_29',['avg_time',['../class_cpp_benchmark_1_1_phase_metrics.html#a7c1eccbcd80fa51b6f7d32aca41e4424',1,'CppBenchmark::PhaseMetrics']]]
 ];
