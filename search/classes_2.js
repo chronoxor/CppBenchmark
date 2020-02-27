9f4da8108c30ab00907d89eb1b48e1a386d1309a
Travis CI <travis@travis-ci.org>
2020-02-27 23:56:21 +0000

    Update generated documentation

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['environment',['Environment',['../class_cpp_benchmark_1_1_environment.html',1,'CppBenchmark']]],
-  ['executor',['Executor',['../class_cpp_benchmark_1_1_executor.html',1,'CppBenchmark']]]
+  ['environment_323',['Environment',['../class_cpp_benchmark_1_1_environment.html',1,'CppBenchmark']]],
+  ['executor_324',['Executor',['../class_cpp_benchmark_1_1_executor.html',1,'CppBenchmark']]]
 ];
