183f8b45daab9694fdbe049943792ae1214ce59f
Travis CI <travis@travis-ci.org>
2020-02-27 23:36:52 +0000

    Update generated documentation

--- search/enumvalues_1.js
+++ search/enumvalues_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cyan',['CYAN',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a344dd8cd533280795b9db82ef0c92749',1,'CppBenchmark']]]
+  ['cyan_631',['CYAN',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a344dd8cd533280795b9db82ef0c92749',1,'CppBenchmark']]]
 ];
