f259f9014cfba66120c9a758ddec673de1357764
Travis CI <travis@travis-ci.org>
2020-02-27 23:53:36 +0000

    Update generated documentation

--- search/all_13.js
+++ search/all_13.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['wait_278',['Wait',['../class_cpp_benchmark_1_1_barrier.html#ada326f3dcf828fc84b1328d7fd2f43c0',1,'CppBenchmark::Barrier']]],
-  ['white_279',['WHITE',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69ab5bf627e448384cf3a4c35121ca6008d',1,'CppBenchmark']]],
-  ['windowsendline_280',['WindowsEndLine',['../class_cpp_benchmark_1_1_environment.html#a8c3597418a3943df248e2bb2a2586a35',1,'CppBenchmark::Environment']]]
+  ['wait',['Wait',['../class_cpp_benchmark_1_1_barrier.html#ada326f3dcf828fc84b1328d7fd2f43c0',1,'CppBenchmark::Barrier']]],
+  ['white',['WHITE',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69ab5bf627e448384cf3a4c35121ca6008d',1,'CppBenchmark']]],
+  ['windowsendline',['WindowsEndLine',['../class_cpp_benchmark_1_1_environment.html#a8c3597418a3943df248e2bb2a2586a35',1,'CppBenchmark::Environment']]]
 ];
