57b8b69d2815bffd8e9b99423470e46fa6893f9c
Travis CI <travis@travis-ci.org>
2020-03-31 16:22:42 +0000

    Update generated documentation

--- search/enums_0.js
+++ search/enums_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['color_627',['Color',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69',1,'CppBenchmark']]]
+  ['color',['Color',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69',1,'CppBenchmark']]]
 ];
--- search/files_6.js
+++ search/files_6.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['reporter_2eh_383',['reporter.h',['../reporter_8h.html',1,'']]],
-  ['reporter_5fconsole_2ecpp_384',['reporter_console.cpp',['../reporter__console_8cpp.html',1,'']]],
-  ['reporter_5fconsole_2eh_385',['reporter_console.h',['../reporter__console_8h.html',1,'']]],
-  ['reporter_5fcsv_2ecpp_386',['reporter_csv.cpp',['../reporter__csv_8cpp.html',1,'']]],
-  ['reporter_5fcsv_2eh_387',['reporter_csv.h',['../reporter__csv_8h.html',1,'']]],
-  ['reporter_5fjson_2ecpp_388',['reporter_json.cpp',['../reporter__json_8cpp.html',1,'']]],
-  ['reporter_5fjson_2eh_389',['reporter_json.h',['../reporter__json_8h.html',1,'']]]
+  ['reporter_2eh',['reporter.h',['../reporter_8h.html',1,'']]],
+  ['reporter_5fconsole_2ecpp',['reporter_console.cpp',['../reporter__console_8cpp.html',1,'']]],
+  ['reporter_5fconsole_2eh',['reporter_console.h',['../reporter__console_8h.html',1,'']]],
+  ['reporter_5fcsv_2ecpp',['reporter_csv.cpp',['../reporter__csv_8cpp.html',1,'']]],
+  ['reporter_5fcsv_2eh',['reporter_csv.h',['../reporter__csv_8h.html',1,'']]],
+  ['reporter_5fjson_2ecpp',['reporter_json.cpp',['../reporter__json_8cpp.html',1,'']]],
+  ['reporter_5fjson_2eh',['reporter_json.h',['../reporter__json_8h.html',1,'']]]
 ];
