56a6dc100a44fc08dc2cc42d32a86cf705f8717c
Travis CI <travis@travis-ci.org>
2020-03-31 20:48:08 +0000

    Update generated documentation

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['y_572',['y',['../class_cpp_benchmark_1_1_context.html#a9a3a359ea78b60c144d82d43454ecddc',1,'CppBenchmark::Context']]]
+  ['y',['y',['../class_cpp_benchmark_1_1_context.html#a9a3a359ea78b60c144d82d43454ecddc',1,'CppBenchmark::Context']]]
 ];
