846ceca590f2c1cdf981d265a26e974d842808fc
Travis CI <travis@travis-ci.org>
2020-05-22 18:57:17 +0000

    Update generated documentation

--- search/enumvalues_8.js
+++ search/enumvalues_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['yellow_643',['YELLOW',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a8a568e5f41b7e4da88fe5c4a00aad34e',1,'CppBenchmark']]]
+  ['yellow',['YELLOW',['../namespace_cpp_benchmark.html#ae3813b3f9ddc837dcb6312cc3f609f69a8a568e5f41b7e4da88fe5c4a00aad34e',1,'CppBenchmark']]]
 ];
