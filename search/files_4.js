8280c27b92f3c142ea1d6e9fd4e6ca8e70284776
Travis CI <travis@travis-ci.org>
2020-05-22 21:26:58 +0000

    Update generated documentation

--- search/files_4.js
+++ search/files_4.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['launcher_2ecpp_371',['launcher.cpp',['../launcher_8cpp.html',1,'']]],
-  ['launcher_2eh_372',['launcher.h',['../launcher_8h.html',1,'']]],
-  ['launcher_5fconsole_2ecpp_373',['launcher_console.cpp',['../launcher__console_8cpp.html',1,'']]],
-  ['launcher_5fconsole_2eh_374',['launcher_console.h',['../launcher__console_8h.html',1,'']]],
-  ['launcher_5fhandler_2eh_375',['launcher_handler.h',['../launcher__handler_8h.html',1,'']]]
+  ['launcher_2ecpp',['launcher.cpp',['../launcher_8cpp.html',1,'']]],
+  ['launcher_2eh',['launcher.h',['../launcher_8h.html',1,'']]],
+  ['launcher_5fconsole_2ecpp',['launcher_console.cpp',['../launcher__console_8cpp.html',1,'']]],
+  ['launcher_5fconsole_2eh',['launcher_console.h',['../launcher__console_8h.html',1,'']]],
+  ['launcher_5fhandler_2eh',['launcher_handler.h',['../launcher__handler_8h.html',1,'']]]
 ];
