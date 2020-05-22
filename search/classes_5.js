fce7ab816554f702e7e661b40cf7ad839e483f6f
Travis CI <travis@travis-ci.org>
2020-05-22 18:47:37 +0000

    Update generated documentation

--- search/classes_5.js
+++ search/classes_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['phase_331',['Phase',['../class_cpp_benchmark_1_1_phase.html',1,'CppBenchmark']]],
-  ['phasecore_332',['PhaseCore',['../class_cpp_benchmark_1_1_phase_core.html',1,'CppBenchmark']]],
-  ['phasemetrics_333',['PhaseMetrics',['../class_cpp_benchmark_1_1_phase_metrics.html',1,'CppBenchmark']]],
-  ['phasescope_334',['PhaseScope',['../class_cpp_benchmark_1_1_phase_scope.html',1,'CppBenchmark']]]
+  ['phase',['Phase',['../class_cpp_benchmark_1_1_phase.html',1,'CppBenchmark']]],
+  ['phasecore',['PhaseCore',['../class_cpp_benchmark_1_1_phase_core.html',1,'CppBenchmark']]],
+  ['phasemetrics',['PhaseMetrics',['../class_cpp_benchmark_1_1_phase_metrics.html',1,'CppBenchmark']]],
+  ['phasescope',['PhaseScope',['../class_cpp_benchmark_1_1_phase_scope.html',1,'CppBenchmark']]]
 ];
