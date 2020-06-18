f972d38af880b779de5d89f0a831ef3b227be32b
Travis CI <travis@travis-ci.org>
2020-06-18 18:46:07 +0000

    Update generated documentation

--- search/all_14.js
+++ search/all_14.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['x_281',['x',['../class_cpp_benchmark_1_1_context.html#a40fef89b43336b12fcf0cfcb72900946',1,'CppBenchmark::Context']]]
+  ['x',['x',['../class_cpp_benchmark_1_1_context.html#a40fef89b43336b12fcf0cfcb72900946',1,'CppBenchmark::Context']]]
 ];
--- search/files_7.js
+++ search/files_7.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['settings_2ecpp_390',['settings.cpp',['../settings_8cpp.html',1,'']]],
-  ['settings_2eh_391',['settings.h',['../settings_8h.html',1,'']]],
-  ['settings_5fpc_2eh_392',['settings_pc.h',['../settings__pc_8h.html',1,'']]],
-  ['settings_5fthreads_2eh_393',['settings_threads.h',['../settings__threads_8h.html',1,'']]],
-  ['system_2ecpp_394',['system.cpp',['../system_8cpp.html',1,'']]],
-  ['system_2eh_395',['system.h',['../system_8h.html',1,'']]]
+  ['settings_2ecpp',['settings.cpp',['../settings_8cpp.html',1,'']]],
+  ['settings_2eh',['settings.h',['../settings_8h.html',1,'']]],
+  ['settings_5fpc_2eh',['settings_pc.h',['../settings__pc_8h.html',1,'']]],
+  ['settings_5fthreads_2eh',['settings_threads.h',['../settings__threads_8h.html',1,'']]],
+  ['system_2ecpp',['system.cpp',['../system_8cpp.html',1,'']]],
+  ['system_2eh',['system.h',['../system_8h.html',1,'']]]
 ];
