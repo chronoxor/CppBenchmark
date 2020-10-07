3a60fb4ae2b2570a2214750a24050a39a091c335
Ivan Shynkarenka <chronoxor@gmail.com>
2020-10-07 22:03:39 +0300

    update

--- modules/zlib.cmake
+++ modules/zlib.cmake
@@ -42,18 +42,16 @@ if(NOT TARGET zlib)
   file(GLOB SOURCE_FILES "zlib/*.c")
   if(MSVC)
     list(APPEND ${SOURCE_FILES} "zlib/contrib/masmx64/inffas8664.c")
-  endif()
-  add_library(zlib ${SOURCE_FILES})
-  if(MSVC)
     # C4127: conditional expression is constant
     # C4131: 'function' : uses old-style declarator
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
     # C4245: 'conversion' : conversion from 'type1' to 'type2', signed/unsigned mismatch
     # C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
-    set_target_properties(zlib PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4131 /wd4244 /wd4245 /wd4267")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4131 /wd4244 /wd4245 /wd4267")
   else()
-    set_target_properties(zlib PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-implicit-function-declaration -Wno-shift-negative-value")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-implicit-function-declaration -Wno-shift-negative-value")
   endif()
+  add_library(zlib ${SOURCE_FILES})
   target_include_directories(zlib PUBLIC "zlib")
   target_link_libraries(zlib)
 
