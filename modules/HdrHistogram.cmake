ac50d55280af6a0018525f27523c7bab5becf8d1
Ivan Shynkarenka <chronoxor@gmail.com>
2020-10-07 18:54:37 +0300

    build

--- modules/HdrHistogram.cmake
+++ modules/HdrHistogram.cmake
@@ -3,19 +3,19 @@ if(NOT TARGET HdrHistogram)
   # Module library
   file(GLOB SOURCE_FILES "HdrHistogram/src/*.c")
   list(FILTER SOURCE_FILES EXCLUDE REGEX ".*/hdr_histogram_log_no_op.c")
+  add_library(HdrHistogram ${SOURCE_FILES})
   if(MSVC)
     # C4057: 'operator' : 'identifier1' indirection to slightly different base types from 'identifier2'
     # C4146: unary minus operator applied to unsigned type, result still unsigned
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4057 /wd4146 /wd4244")
+    set_target_properties(HdrHistogram PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4057 /wd4146 /wd4244")
   else()
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-char-subscripts -Wno-format -Wno-implicit-function-declaration -Wno-incompatible-pointer-types")
+    set_target_properties(HdrHistogram PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-char-subscripts -Wno-format -Wno-implicit-function-declaration -Wno-incompatible-pointer-types")
   endif()
-  add_library(HdrHistogram ${SOURCE_FILES})
   target_include_directories(HdrHistogram PUBLIC "HdrHistogram/src")
   target_link_libraries(HdrHistogram zlib)
 
   # Module folder
-  set_target_properties(HdrHistogram PROPERTIES FOLDER modules/HdrHistogram)
+  set_target_properties(HdrHistogram PROPERTIES FOLDER "modules/HdrHistogram")
 
 endif()
--- modules/zlib.cmake
+++ modules/zlib.cmake
@@ -41,21 +41,23 @@ if(NOT TARGET zlib)
   # Module library
   file(GLOB SOURCE_FILES "zlib/*.c")
   if(MSVC)
+    list(APPEND ${SOURCE_FILES} "zlib/contrib/masmx64/inffas8664.c")
+  endif()
+  add_library(zlib ${SOURCE_FILES} ${ASSEMBLER_FILES})
+  if(MSVC)
     # C4127: conditional expression is constant
     # C4131: 'function' : uses old-style declarator
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
     # C4245: 'conversion' : conversion from 'type1' to 'type2', signed/unsigned mismatch
     # C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
-    set(${SOURCE_FILES} "zlib/contrib/masmx64/inffas8664.c" "zlib/contrib/masmx64/inffas8664.c")
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4131 /wd4244 /wd4245 /wd4267")
+    set_target_properties(zlib PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4131 /wd4244 /wd4245 /wd4267")
   else()
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-implicit-function-declaration -Wno-shift-negative-value")
+    set_target_properties(zlib PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-implicit-function-declaration -Wno-shift-negative-value")
   endif()
-  add_library(zlib ${SOURCE_FILES} ${ASSEMBLER_FILES})
   target_include_directories(zlib PUBLIC "zlib")
   target_link_libraries(zlib)
 
   # Module folder
-  set_target_properties(zlib PROPERTIES FOLDER modules/zlib)
+  set_target_properties(zlib PROPERTIES FOLDER "modules/zlib")
 
 endif()
