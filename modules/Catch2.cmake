8be8d8f0d50ec6bf4fd32e21777d6201f92123a1
Ivan Shynkarenka <chronoxor@gmail.com>
2023-01-25 23:27:35 +0400

    Catch2: Migrating from v2 to v3

--- modules/Catch2.cmake
+++ modules/Catch2.cmake
@@ -1,10 +1,18 @@
 if(NOT TARGET Catch2)
 
+  # Restore origin compile flags
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_ORIGIN}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_ORIGIN}")
+
   # Module subdirectory
   add_subdirectory("Catch2")
 
   # Module folder
   set_target_properties(Catch2 PROPERTIES FOLDER "modules/Catch2")
   set_target_properties(Catch2WithMain PROPERTIES FOLDER "modules/Catch2")
+
+  # Restore custom compile flags
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_CUSTOM}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_CUSTOM}")
 
 endif()
