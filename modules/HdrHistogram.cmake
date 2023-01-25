cb13862d8084ec9821e657655afe3d92f57c60d9
Ivan Shynkarenka <chronoxor@gmail.com>
2023-01-25 22:53:48 +0400

    Catch2: Migrating from v2 to v3

--- modules/HdrHistogram.cmake
+++ modules/HdrHistogram.cmake
@@ -10,7 +10,7 @@ if(NOT TARGET HdrHistogram)
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
     set_target_properties(HdrHistogram PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4057 /wd4146 /wd4244")
   elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set_target_properties(HdrHistogram PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-restrict")
+    set_target_properties(HdrHistogram PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-char-subscripts -Wno-format -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-restrict")
   else()
     set_target_properties(HdrHistogram PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
   endif()
