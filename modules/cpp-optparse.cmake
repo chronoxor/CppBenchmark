eee17a6e1e8dd9f898c598eeaeddedf11d2c1414
chronoxor <chronoxor@gmail.com>
2025-07-15 16:41:45 +0200

    Fix gcc 15 deprecated warnings

--- modules/cpp-optparse.cmake
+++ modules/cpp-optparse.cmake
@@ -7,6 +7,9 @@ if(NOT TARGET cpp-optparse)
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
     # C4996: <header> is removed in C++20
     set_target_properties(cpp-optparse PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4244 /wd4996")
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    # Warning "<ciso646> is deprecated in C++17, use <version> to detect implementation-specific macros
+    set_target_properties(cpp-optparse PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wdeprecated-declarations")
   else()
     set_target_properties(cpp-optparse PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
   endif()
